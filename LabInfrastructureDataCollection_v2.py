7436a57d4303afd87b7841dcc950d2607b455249
bhearley <109361610+bhearley@users.noreply.github.com>
2023-12-20 09:42:48 -0500

    Update LabInfrastructureDataCollection_v2.py

--- LabInfrastructureDataCollection_v2.py
+++ LabInfrastructureDataCollection_v2.py
@@ -20,7 +20,7 @@ import streamlit as st
 # Set Paths
 home = os.getcwd()
 data_path = "./NewFiles"
-data_template = ps.path.join(home,"./Template/DataTemplate.pkl")
+data_template = os.path.join(home,"Template","DataTemplate.pkl")
 
 #----------------------------------------------------------------
 #   CREATE THE APP
