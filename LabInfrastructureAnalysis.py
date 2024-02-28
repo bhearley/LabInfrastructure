633356ec01937c1cba133451891c562efe126204
bhearley <109361610+bhearley@users.noreply.github.com>
2024-02-28 16:29:39 -0500

    Update LabInfrastructureAnalysis.py

--- LabInfrastructureAnalysis.py
+++ LabInfrastructureAnalysis.py
@@ -1,5 +1,12 @@
 # Import Modules
 import streamlit as st
+import os
+import glob
+
+# Set Paths
+home = os.getcwd()
+data_path = "/mount/src/labinfrastructure/Final/"
+
 
 # Set the page configuration
 st.set_page_config(layout="wide")
@@ -9,3 +16,23 @@ st.title("NASA GRC Laboratory Infrastructure Data Collection Analysis")
 
 #Create Divider for Name and Description
 st.subheader('Set Filter Criteria')
+
+# Get List of Divisions and Branches
+os.chdir(data_path)
+files_all = glob.glob('*.txt')
+
+for q in range(len(files_all)):
+    # Read the Text File
+    with open(os.path.join(data_path,files_all[q])) as f:
+        lines = f.readlines()
+
+    # Get the Branch Name
+    key = 'Branch:'
+    for i in range(len(lines)):
+        if key in lines[i]:
+            val  = lines[i][len(key)+1:len(lines[i])-1]
+
+    if len(val) == 3:
+        Direc = val[0]
+        Div = val[0:2]
+        Branch = val[0:3]
