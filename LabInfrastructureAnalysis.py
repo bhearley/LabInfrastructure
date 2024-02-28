305444322d4bfa17ede47ac1bd81a94e9eadcb6c
bhearley <109361610+bhearley@users.noreply.github.com>
2024-02-28 16:26:33 -0500

    Update LabInfrastructureAnalysis.py

--- LabInfrastructureAnalysis.py
+++ LabInfrastructureAnalysis.py
@@ -6,3 +6,6 @@ st.set_page_config(layout="wide")
 
 # Create the Title
 st.title("NASA GRC Laboratory Infrastructure Data Collection Analysis")
+
+#Create Divider for Name and Description
+st.subheader('Set Filter Criteria')
