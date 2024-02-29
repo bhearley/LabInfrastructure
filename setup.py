6cb3225659475ad286956a0f5afb333a61cf0030
bhearley <109361610+bhearley@users.noreply.github.com>
2024-02-29 12:38:50 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -8,6 +8,6 @@ setup(
     packages = find_packages(),
     install_requires = [
         # Github Private Repository
-        'LabInfrastructure @ git+https://github.com/bhearley/LabInfrastructure.git'
+        #'LabInfrastructure @ git+https://github.com/bhearley/LabInfrastructure.git'
     ]
 )
