9a721ca549b1b60314a260d426ec1c14398eb2c3
bhearley <109361610+bhearley@users.noreply.github.com>
2024-03-19 11:59:22 -0400

    Update LabInfrastructureDataCollection_v2.py

--- LabInfrastructureDataCollection_v2.py
+++ LabInfrastructureDataCollection_v2.py
@@ -589,7 +589,7 @@ css = '''
         width: max-content;
     }
     [data-testid='stFileUploader'] section {
-        padding: 10;
+        padding: 1;
         float: left;
     }
     [data-testid='stFileUploader'] section > input + div {
