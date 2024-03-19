c9d4aef89375c7f87ba3c36cedb9e8c218db3715
bhearley <109361610+bhearley@users.noreply.github.com>
2024-03-19 11:31:26 -0400

    Update LabInfrastructureDataCollection_v2.py

--- LabInfrastructureDataCollection_v2.py
+++ LabInfrastructureDataCollection_v2.py
@@ -564,11 +564,13 @@ def add_row_img(row):
             asset_imgs_lab.append(None)
         if row == 0:
             asset_imgs_lab[row]=st.selectbox('Asset', options_dt, key=f'input_colimg1{row}')
+            s = f"<p style='font-size:45px; color:white'>{'TEST'}</p>"
+            st.markdown(s, unsafe_allow_html=True)
+            
         else:
             asset_imgs_lab[row]=st.selectbox('Asset Image #' + str(row+1), options_dt, key=f'input_colimg1{row}')
 
-            s = f"<p style='font-size:45px;'>{'TEST'}</p>"
-            
+            s = f"<p style='font-size:45px; color:white'>{'TEST'}</p>"
             st.markdown(s, unsafe_allow_html=True)
     # -- Asset image   
     with grid_img[1]:
