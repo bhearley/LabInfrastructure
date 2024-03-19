9e5c500a94fc84ebf4b68fba013bbaf09db8aa95
bhearley <109361610+bhearley@users.noreply.github.com>
2024-03-19 11:12:11 -0400

    Update LabInfrastructureDataCollection_v2.py

--- LabInfrastructureDataCollection_v2.py
+++ LabInfrastructureDataCollection_v2.py
@@ -565,26 +565,14 @@ def add_row_img(row):
         if row == 0:
             asset_imgs_lab[row]=st.selectbox('Asset', options_dt, key=f'input_colimg1{row}')
         else:
-            def color_selectbox(n_element:int, color:str):
-
-                js = f'''
-                <script>
-                // Find all the selectboxes
-                var selectboxes = window.parent.document.getElementsByClassName("stSelectbox");
-                
-                // Select one of them
-                var selectbox = selectboxes[{n_element}];
-                
-                // Select only the selection div
-                var selectregion = selectbox.querySelector('[data-baseweb="select"]');
-                
-                // Modify the color
-                selectregion.style.backgroundColor = '{color}';
-                </script>
-                '''
-                st.components.v1.html(js, height=0)
+            tabs_font_css = """
+            <style>
+            div[class*="stSelectBox"] label {
+              font-size: 26px;
+            }
+            </style>
+            """
             asset_imgs_lab[row]=st.selectbox('Asset Image #' + str(row+1), options_dt, key=f'input_colimg1{row}')
-            color_selectbox(0, 'pink')
     # -- Asset image   
     with grid_img[1]:
         while len(asset_imgs) < row+1:
