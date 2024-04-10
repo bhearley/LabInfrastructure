a40bf8d2365f635e0c85e4ff4a7850eb5b644e72
bhearley <109361610+bhearley@users.noreply.github.com>
2024-04-10 11:16:37 -0400

    Update LabInfrastructureDataConnected.py

--- LabInfrastructureDataConnected.py
+++ LabInfrastructureDataConnected.py
@@ -2,24 +2,14 @@ import streamlit as st
 import pymongo
 
 
-client = pymongo.MongoClient(**st.secrets["mongo"])
+@st.cache_resource
+def init_connection():
+    connection_string = st.secrets["mongo"]["connection_string"]
+    return pymongo.MongoClient(connection_string)
+
+client = init_connection()
 try:
     client.admin.command('ping')
     st.markdown("Pinged your deployment. You successfully connected to MongoDB!")
 except Exception as e:
     st.markdown(e)
-
-# Pull data from the collection.
-# Uses st.cache_data to only rerun when the query changes or after 10 min.
-#@st.cache_data(ttl=600)
-#def get_data():
-#    db = client.testdb
-#    items = db.mycollection.find()
-#    items = list(items)  # make hashable for st.cache_data
-#    return items
-
-#items = get_data()
-
-# Print results.
-#for item in items:
-#    st.write(item["name"])
