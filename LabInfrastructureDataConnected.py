bf173cf529497c8d574e6b384cd0eb5eae1291ef
bhearley <109361610+bhearley@users.noreply.github.com>
2024-04-10 10:20:38 -0400

    Update LabInfrastructureDataConnected.py

--- LabInfrastructureDataConnected.py
+++ LabInfrastructureDataConnected.py
@@ -1,23 +1,29 @@
 # Import Modules
 import streamlit as st
-
-import pymongo
-
-# Initialize connection.
-# Uses st.cache_resource to only run once.
-@st.cache_resource
-def init_connection():
-    return pymongo.MongoClient(**st.secrets["mongo"])
-
-client = init_connection()
+from pymongo.mongo_client import MongoClient
+uri = "mongodb+srv://nasagrc:brookpark21000@nasagrclabdatatest.hnx1ick.mongodb.net/?retryWrites=true&w=majority&appName=NASAGRCLabDataTest"
+# Create a new client and connect to the server
+client = MongoClient(uri)
+# Send a ping to confirm a successful connection
+try:
+    client.admin.command('ping')
+    print("Pinged your deployment. You successfully connected to MongoDB!")
+except Exception as e:
+    print(e)
 
 db = client['testdb']
 # Access the 'example_collection' collection
 collection = db['example_collection']
 
+# Prepare a document to insert
+#document = {
+#    'name': 'John Doe',
+#    'age': 30,
+#    'email': 'john@example.com'
+#}
+
 # Query for documents with age greater than 25
 query = {'name': 'John Doe'}
 results = collection.find(query)
-# Print the matching documents
 for result in results:
-    st.markdown(result['name'])
+    st.markdown(results)
