bb43a03632cd31816487de33df719b33f52fcf79
bhearley <109361610+bhearley@users.noreply.github.com>
2024-04-10 12:12:35 -0400

    Update LabInfrastructureDataConnected.py

--- LabInfrastructureDataConnected.py
+++ LabInfrastructureDataConnected.py
@@ -4,18 +4,26 @@ import pymongo
 
 #write data in a file.
 
-temp_file = 
+temp_file = "/mount/src/labinfrastructure/TestData.txt'
 
 file1 = open(temp_file, "w")
+content = file1.read()
+
+st.markdown(content)
+
+
 L = ["This is Delhi \n", "This is Paris \n", "This is London \n"]
- 
 # \n is placed to indicate EOL (End of Line)
 file1.write("Hello \n")
 file1.writelines(L)
 file1.close()  # to change file access modes
 
-@st.cache_resource
-def init_connection():
-    return pymongo.MongoClient(**st.secrets["mongo"])
+file1 = open(temp_file, "w")
+
+st.markdown(content)
+
+#@st.cache_resource
+#def init_connection():
+#    return pymongo.MongoClient(**st.secrets["mongo"])
 
 
