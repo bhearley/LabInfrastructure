a3c49ceed7091838f0e2b4681e47190f0eb02e13
bhearley <109361610+bhearley@users.noreply.github.com>
2024-04-10 11:30:17 -0400

    Update LabInfrastructureDataConnected.py

--- LabInfrastructureDataConnected.py
+++ LabInfrastructureDataConnected.py
@@ -2,14 +2,16 @@ import streamlit as st
 import pymongo
 
 
-@st.cache_resource
-def init_connection():
-    connection_string = st.secrets["mongo"]["connection_string"]
-    return pymongo.MongoClient(connection_string)
+st.markdown(st.secrets["mongo"]["connection_string"]
 
-client = init_connection()
-try:
-    client.admin.command('ping')
-    st.markdown("Pinged your deployment. You successfully connected to MongoDB!")
-except Exception as e:
-    st.markdown(e)
+#@st.cache_resource
+#def init_connection():
+#    connection_string = st.secrets["mongo"]["connection_string"]
+#    return pymongo.MongoClient(connection_string)
+
+#client = init_connection()
+#try:
+#    client.admin.command('ping')
+#    st.markdown("Pinged your deployment. You successfully connected to MongoDB!")
+#except Exception as e:
+#    st.markdown(e)
