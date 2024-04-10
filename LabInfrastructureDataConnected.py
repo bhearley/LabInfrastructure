937c95129ca38ee9369c051c3daa1bcb2c79a443
bhearley <109361610+bhearley@users.noreply.github.com>
2024-04-10 12:29:12 -0400

    Update LabInfrastructureDataConnected.py

--- LabInfrastructureDataConnected.py
+++ LabInfrastructureDataConnected.py
@@ -9,3 +9,10 @@ df = conn.read()
 # Print results.
 for row in df.itertuples():
     st.write(f"{row.name} has a :{row.pet}:")
+
+if st.button('Submit'):
+    df = conn.create(
+            worksheet="Example 1",
+            data=['a','b'],
+        )
+        st.cache_data.clear()
