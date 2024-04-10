c99c5a9daec9cd8c058a5fefbfad3117f3ee8a6d
bhearley <109361610+bhearley@users.noreply.github.com>
2024-04-10 13:24:13 -0400

    Update LabInfrastructureDataConnected.py

--- LabInfrastructureDataConnected.py
+++ LabInfrastructureDataConnected.py
@@ -7,7 +7,7 @@ uri = "mongodb+srv://nasagrc:brookpark21000@nasagrclabdatatest.hnx1ick.mongodb.n
 # Create a new client and connect to the server
 #client = MongoClient(uri)
 
-client = MongoClient(**st.secrets["mongo1"])
+client = MongoClient(**st.secrets["mongo2"])
 #client = MongoClient(uri, tlsCAFile=certifi.where())
 # Send a ping to confirm a successful connection
 try:
