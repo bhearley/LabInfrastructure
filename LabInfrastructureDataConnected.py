2491ee6cbb207372e3d38f8154f775cb7a790bb1
bhearley <109361610+bhearley@users.noreply.github.com>
2024-04-10 11:42:40 -0400

    Update LabInfrastructureDataConnected.py

--- LabInfrastructureDataConnected.py
+++ LabInfrastructureDataConnected.py
@@ -7,14 +7,20 @@ st.markdown(st.secrets["mongo"]["port"])
 st.markdown(st.secrets["mongo"]["username"])
 st.markdown(st.secrets["mongo"]["password"])
 
+@st.cache_resource
+def init_connection():
+    return pymongo.MongoClient(**st.secrets["mongo"])
+
+client = init_connection()
+
 #@st.cache_resource
 #def init_connection():
 #    connection_string = st.secrets["mongo"]["connection_string"]
 #    return pymongo.MongoClient(connection_string)
 
 #client = init_connection()
-#try:
-#    client.admin.command('ping')
-#    st.markdown("Pinged your deployment. You successfully connected to MongoDB!")
-#except Exception as e:
-#    st.markdown(e)
+try:
+    client.admin.command('ping')
+    st.markdown("Pinged your deployment. You successfully connected to MongoDB!")
+except Exception as e:
+    st.markdown(e)
