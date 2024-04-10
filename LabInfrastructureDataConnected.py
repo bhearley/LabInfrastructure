ed8f998c5a6effa609c44330fa20a961a92e7945
bhearley <109361610+bhearley@users.noreply.github.com>
2024-04-10 15:02:33 -0400

    Update LabInfrastructureDataConnected.py

--- LabInfrastructureDataConnected.py
+++ LabInfrastructureDataConnected.py
@@ -1,20 +1,34 @@
+# Import Modues
 import streamlit as st
 from pymongo.mongo_client import MongoClient
 import dns
 import certifi
 
+# Connect to the Database
 uri = "mongodb+srv://nasagrc:brookpark21000@nasagrclabdatatest.hnx1ick.mongodb.net/?retryWrites=true&w=majority&appName=NASAGRCLabDataTest"
-# Create a new client and connect to the server
-#client = MongoClient(uri)
-
-#client = MongoClient(**st.secrets["mongo2"])
 client = MongoClient(uri, tlsCAFile=certifi.where())
 # Send a ping to confirm a successful connection
 try:
     client.admin.command('ping')
-    st.markdown("Pinged your deployment. You successfully connected to MongoDB!")
+    print("Pinged your deployment. You successfully connected to MongoDB!")
 except Exception as e:
-    st.markdown(e)
+    print(e)
+
+# Get List of Existing Records
+db = client['LabData']
+collection = db['LabData']
+cursor = collection.find({})
+Rec_Exist = {}
+for document in cursor:
+    # Get the current list of all branches
+    cur_branch = list(Rec_Exist.keys())
+    # -- Add branch if new
+    if document["Branch"] not in cur_branch:
+        Rec_Exist[document["Branch"]] = []
+
+    # Add record to branch list
+    Rec_Exist[document['Branch']].append(document["Laboratory/Capability Name"])
+    st.markdown(document)
 
 if st.button('View Data'):
     db = client['testdb']
