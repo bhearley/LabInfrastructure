36691ee7ac12062996eb9fa242ae6e25c51458b9
bhearley <109361610+bhearley@users.noreply.github.com>
2024-04-10 15:37:54 -0400

    Update LabInfrastructureDataConnected.py

--- LabInfrastructureDataConnected.py
+++ LabInfrastructureDataConnected.py
@@ -49,8 +49,8 @@ for document in cursor:
     Rec_Exist[document['Branch']].append(document["Laboratory/Capability Name"])
 
     # Initialize Lists
-    selection_branch_list  =list(Rec_Exist.keys())
-    selection_lab_list = Rec_Exist[selection_branch_list[0]]
+    selection_branch_list  = ['',list(Rec_Exist.keys())]
+    selection_lab_list = ['', Rec_Exist[selection_branch_list[0]]]
     
     # Create the Function to get the list of branches
     def get_selection_lab():
