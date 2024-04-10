5d9fd29ebb5ef9c77f7b64b6eb4a902f67ff2146
bhearley <109361610+bhearley@users.noreply.github.com>
2024-04-10 15:16:58 -0400

    Update LabInfrastructureDataConnected.py

--- LabInfrastructureDataConnected.py
+++ LabInfrastructureDataConnected.py
@@ -4,6 +4,24 @@ from pymongo.mongo_client import MongoClient
 import dns
 import certifi
 
+# Set the page configuration
+st.set_page_config(layout="wide")
+
+# Create the Title
+st.title("NASA GRC Laboratory Infrastructure Data Collection")
+
+# Create Instructions
+st.markdown('The NASA GRC Laboratory Infrastructure Data Collection Tool will capture the current state of GRC capabilities. ' +
+            'This information is necessary to assess the overall state of our infrastructure and assets and will be used to develop ' +
+            'strategic plans for laboratory investment. \n \n \n' 
+            'Please complete each of the fields below for Laboratory assets.  \n\n' +
+            '  - A Laboratory is defined as a dedicated facility, or dedicated infrastructure, for performing a specific type of testing, ' +
+            'research, or development. A laboratory may encompass a unique capability, and may include multiple high values assets such as ' +
+            'test or analytical equipment. (i.e. The Structural Dynamics Laboratory) \n\n' 
+            '  - An asset is defined as a unique equipment that is segregable from the facility. An asset may be composed of multiple components. (i.e. a Scanning Electron Microscope). Consider only assets associated with the infrastructure of the lab and not the facility. \n\n' + 
+            '  - For each laboratory enter assets with a value over $50K or assets at lower values that are extremely critical or different to replace. \n\n \n'+
+           'For questions regarding the data collection tool, please contact Brandon Hearley (LMS) at brandon.l.hearley@nasa.gov')
+
 # Connect to the Database
 uri = "mongodb+srv://nasagrc:brookpark21000@nasagrclabdatatest.hnx1ick.mongodb.net/?retryWrites=true&w=majority&appName=NASAGRCLabDataTest"
 client = MongoClient(uri, tlsCAFile=certifi.where())
@@ -45,7 +63,33 @@ for document in cursor:
         with selection_grid[1]:
             selection_lab = st.selectbox('Select the Branch:',selection_lab_list, key = 'selection_lab') 
 
+#Create Divider for Name and Description
+st.subheader('Laboratory Information')
 
+# Create Input for Asset Name
+lab_name = st.text_input("Laboratory Name:",value='',key='name')
+
+# Create Input for Point of Contact
+poc = st.text_input("Point of Contact:",value='', key = 'poc')
+
+# Create Input for Branch
+branch = st.text_input("Branch:",value='', key = 'branch')
+
+# Create Input for Asset Description
+lab_desc = st.text_area("Laboratory/Capability Description:",value='',key='desc')
+
+# Create Input for Asset Website
+lab_link = st.text_input("Laboratory/Capability Website:",value='',key='link')
+
+# Create Input for Describing Challegne in Sustaining
+lab_chal = st.text_area("Challenges in sustaining this laboratory/capability:",value='',key='chal')
+
+# Create Input for Age
+lab_age = st.number_input("Age (yrs):",min_value=0,max_value=None,value=0,help="The age of the laboratory/capability (i.e., how long we've had this capability at NASA GRC)",key='lab_age_k')
+
+# Create Input for Condition
+cond_opts = ['Excellent','Good','Fair','Poor']
+lab_condition = st.selectbox('Condition:',cond_opts,key='cond')
 
 
 
