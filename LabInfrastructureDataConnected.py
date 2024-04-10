3389ee63afa520803428c00bdfd095cdf163431b
bhearley <109361610+bhearley@users.noreply.github.com>
2024-04-10 12:43:59 -0400

    Update LabInfrastructureDataConnected.py

--- LabInfrastructureDataConnected.py
+++ LabInfrastructureDataConnected.py
@@ -8,6 +8,6 @@ client = MongoClient(uri)
 # Send a ping to confirm a successful connection
 try:
     client.admin.command('ping')
-    print("Pinged your deployment. You successfully connected to MongoDB!")
+    st.markdown("Pinged your deployment. You successfully connected to MongoDB!")
 except Exception as e:
-    print(e)
+    st.markdown(e)
