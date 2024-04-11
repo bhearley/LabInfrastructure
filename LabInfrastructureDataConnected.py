51234f7d8f5d559264168f50a596afd6e396ff94
bhearley <109361610+bhearley@users.noreply.github.com>
2024-04-11 09:14:58 -0400

    Update LabInfrastructureDataConnected.py

--- LabInfrastructureDataConnected.py
+++ LabInfrastructureDataConnected.py
@@ -69,6 +69,8 @@ def load_data():
             st.session_state['lab_age'] = result['Age (yrs)']
             st.session_state['cond'] = result['Condition'].strip()
             st.session_state['asset_num'] = result['Number of Assets']
+            for m in range(int(result['Number of Assets'])):
+                st.session_state[f'input_cola{k}'] = result['Asset Name'][m]
 
             
             st.session_state['test_area'] = '--' + result['Condition'].strip() + '--'
