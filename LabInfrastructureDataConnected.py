d81b0ebc5ac48d479690863a11065e3f0fd9c3aa
bhearley <109361610+bhearley@users.noreply.github.com>
2024-04-11 15:12:09 -0400

    Update LabInfrastructureDataConnected.py

--- LabInfrastructureDataConnected.py
+++ LabInfrastructureDataConnected.py
@@ -639,7 +639,7 @@ def add_row5(row):
 for r in range(int(labor_rows)):
     add_row5(r)
 
-grid_db = st.columns(2)
+grid_db = st.columns([0.1,0.1,0.8])
 with grid_db[0]:
     if st.button('Save To Database'):
         # Create New Document
