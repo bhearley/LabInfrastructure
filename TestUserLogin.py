66486ac87d4889fa8584abd2afcb3fde9222d1c5
bhearley <109361610+bhearley@users.noreply.github.com>
2024-04-23 12:31:06 -0400

    Update TestUserLogin.py

--- TestUserLogin.py
+++ TestUserLogin.py
@@ -155,9 +155,9 @@ def create_home_screen(home_screen):
                     st.error('Username is unavailable. If you have an account, please enter your password.')
 
   
-#if "user_key" not in st.session_state:
-if 'sign_up_btn_2' not in st.session_state:
-    st.write('Test')    
+if "user_key" not in st.session_state:
+    if 'sign_up_btn_2' not in st.session_state:
+        st.write('Test')    
     create_home_screen(1)
 else:
     if st.session_state['sign_up_btn_1'] == True:
