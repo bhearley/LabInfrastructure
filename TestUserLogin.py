54bac8711e4fc619279f2042d2dc18f8df2984e3
bhearley <109361610+bhearley@users.noreply.github.com>
2024-04-29 10:56:07 -0400

    Update TestUserLogin.py

--- TestUserLogin.py
+++ TestUserLogin.py
@@ -425,7 +425,7 @@ if access == 'Yes':
     
     # Create Input for Assets
     asset_rows = st.number_input('Number of Assets:', min_value=0, max_value=None, key='asset_num')
-    grid_asset = st.columns([0.125,0.070,0.060,0.065,0.09,0.08,0.07,0.11,0.115,0.07,0.1])
+    grid_asset = st.columns([0.125,0.070,0.058,0.067,0.09,0.08,0.07,0.11,0.115,0.07,0.1])
     asset_name = [] #Store the asset name
     asset_loc = []  #Store the asset location
     asset_age = [] #Store the asset age
