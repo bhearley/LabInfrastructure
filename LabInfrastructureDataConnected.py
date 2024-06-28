d46448c8c242edeb29aca5ff0f407a99c7b74330
bhearley <109361610+bhearley@users.noreply.github.com>
2024-06-28 10:50:35 -0400

    Update LabInfrastructureDataConnected.py

--- LabInfrastructureDataConnected.py
+++ LabInfrastructureDataConnected.py
@@ -295,9 +295,7 @@ if access == 'Yes':
     # Read the Database
     @st.cache_data(ttl=6000)
     def get_data():
-        st.write(client['LabData'])
-        # st.write('Loading Data')
-        # db = client['LabData']
+        db = client['LabData']
         # items = db['LabData'].find()
         # items = list(items)  # make hashable for st.cache_data
         # st.write('Loaded Data')
