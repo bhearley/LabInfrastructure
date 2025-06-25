a0baeca434811d47d9a5119da007eb88a67e43f7
bhearley <109361610+bhearley@users.noreply.github.com>
2025-06-25 12:59:29 -0400

    Update LabInfrastructureAnalysisConnected.py

--- LabInfrastructureAnalysisConnected.py
+++ LabInfrastructureAnalysisConnected.py
@@ -693,17 +693,11 @@ if st.button('Filter Data'):
                         for k in range(len(col_keys)):
                             try:
                                 if col_keys[k] == 'T1-Location (Bldg/Rm)' and len(record['T1-Location (Bldg/Rm)']) == 0:
-                                    st.write(record['Laboratory/Capability Name'])
-                                    st.write(record['T1-Building'])
-                                    st.write(record['T1-Room'])
-                                    
-                                val = record[col_keys[k]][j]
-
-                                
+                                    val = str(record['T1-Building']) + str(record['T1-Room'])
+                                else:
+                                    val = record[col_keys[k]][j]
                                     
                                 val_frmt = format_values(val, col_dict[col_keys[k]])
-
-                                
                                 
                                 row[k].text = val_frmt
                             except:
